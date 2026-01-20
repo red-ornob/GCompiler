2854b7e1fad0858d67b6d94c097e54fb366fbc4e
red-ornob <red.ornob.dev@gmail.com>
2026-01-20 06:04:32 +0600

    added float, imaginary, keyword and comment lexing

--- Frontend/Lexer.cs
+++ Frontend/Lexer.cs
@@ -1,3 +1,5 @@
+using System.Text;
+
 namespace Frontend;
 
 internal class Lexer(string filePath): IDisposable
@@ -8,8 +10,8 @@ internal class Lexer(string filePath): IDisposable
     private int _lineCount;
     private int _charNum;
     
-    private char CurrChar => _line[_charNum];
-    private char NextChar => _line[_charNum + 1];
+    private char CurrChar => _charNum < _line.Length ? _line[_charNum] : '\0';
+    private char NextChar => _charNum + 1 < _line.Length ? _line[_charNum + 1] : '\0';
     private string Position => $"{_lineCount}:{_charNum + 1} at {filePath}";
     public bool EndOfStream => _fs.EndOfStream;
     
@@ -28,9 +30,15 @@ internal class Lexer(string filePath): IDisposable
                 continue;
             }
             
-            if (char.IsDigit(CurrChar))
+            if (char.IsDigit(CurrChar) || CurrChar == '.')
             {
                 _tokenList.Add(LexIntegerLiteral());
+                continue;
+            }
+            
+            if (CurrChar == '/' && NextChar is '/' or '*')
+            {
+                _tokenList.Add(LexComment());
                 continue;
             }
             
@@ -57,31 +65,53 @@ internal class Lexer(string filePath): IDisposable
         var length = 0;
         for (; _charNum < _line.Length && (char.IsLetterOrDigit(CurrChar) || CurrChar == '_'); _charNum++) length++;
         _charNum--;
-        return new Token(TokenType.Identifier, _line.Substring(startIndex, length));
+        
+        Span<String> keywords = [
+            "break", "default", "func", "interface", "select",
+            "case", "defer", "go", "map", "struct",
+            "chan", "else", "goto", "package", "switch",
+            "const", "fallthrough", "if", "range", "type",
+            "continue", "for", "import", "return", "var"
+        ];
+        
+        if (keywords.Contains(_line.Substring(startIndex, length))) return new(TokenType.Keyword, _line.Substring(startIndex, length));
+        
+        return new(TokenType.Identifier, _line.Substring(startIndex, length));
     }
     
     private Token LexIntegerLiteral()
     {
         var startIndex = _charNum;
         var length = 0;
+        var isFloat = false;
         var currBase = '0';
-        if (CurrChar == '0' && char.IsLetter(CurrChar))
+        Span<char> digitBases = ['b', 'o', 'x'];
+        if (CurrChar == '0' && digitBases.Contains(char.ToLower(NextChar)))
         {
-            length++;
-            _charNum++;
-            currBase = char.ToLower(CurrChar);
+            currBase = char.ToLower(NextChar);
+            length += 2;
+            _charNum += 2;
         }
         
-        for (; _charNum < _line.Length && (InBase(CurrChar, currBase) || CurrChar == '_'); _charNum++)
+        for (; _charNum < _line.Length 
+               && (InBase(CurrChar, currBase) || CurrChar == '_' || CurrChar == '.'); _charNum++)
         {
             length++;
-            if (CurrChar == '_' && !InBase(NextChar, currBase))
-            {
+            if ((CurrChar == '_' && !InBase(NextChar, currBase)) || (NextChar == '_' && !InBase(CurrChar, currBase))) 
                 throw new LexerException($"Invalid integer literal: {Position}");
+            if (CurrChar == '.')
+            {
+                if (isFloat) throw new LexerException($"Invalid integer literal: {Position}");
+                isFloat = true;
             }
         }
+        
+        if (_charNum < _line.Length && CurrChar == 'i') 
+            return new(TokenType.ImaginaryLiteral, _line.Substring(startIndex, ++length));
         _charNum--;
-        return new Token(TokenType.IntegerLiteral, _line.Substring(startIndex, length));
+        
+        if (isFloat) return new(TokenType.FloatingPointLiteral, _line.Substring(startIndex, length));
+        return new(TokenType.IntegerLiteral, _line.Substring(startIndex, length));
     }
     
     private bool InBase(char intChar, char baseChar)
@@ -99,6 +129,36 @@ internal class Lexer(string filePath): IDisposable
             'x' => hexDigits.Contains(char.ToUpper(intChar)),
             _ => throw new LexerException($"Invalid base character: {Position}")
         };
+    }
+    
+    private Token LexComment()
+    {
+        var startIndex = _charNum;
+        if (NextChar == '/')
+        {
+            _charNum = _line.Length;
+            return new(TokenType.Comment, _line.Substring(startIndex));
+        }
+        
+        var stringBuilder = new StringBuilder();
+        
+        if (!_line.Contains("*/"))
+        {
+            stringBuilder.Append(_line.Substring(startIndex));
+            _line = _fs.ReadLine() ?? string.Empty;
+            _lineCount++;
+        }
+        while (!_line.Contains("*/") && !EndOfStream)
+        {
+            stringBuilder.Append(_line);
+            _line = _fs.ReadLine() ?? string.Empty;
+            _lineCount++;
+        }
+        
+        _charNum = _line.IndexOf("*/", StringComparison.Ordinal) + 1;
+        if (_charNum == 0) throw new LexerException($"Invalid comment {Position}");
+        stringBuilder.Append(_line.Substring(0, _charNum + 1));
+        return new(TokenType.Comment, stringBuilder.ToString());
     }
     
     public void Dispose()
--- Frontend/test.g
+++ Frontend/test.g
@@ -3,10 +3,27 @@ _x9
 ThisVariableIsExported
 αβ
 
+break default func interface select
+case defer go map struct
+chan else goto package switch
+const fallthrough if range type
+continue for import return var
+
++ & += &= && == != ( )
+- | -= |= || < <= [ ]
+* ^ *= ^= <- > >= { }
+/ << /= <<= ++ = := , ;
+% >> %= >>= -- ! ... . :
+&^ &^= ~
+
 ;
 ;;
 semicolon;
-test; 67;
+test; 67; // this is a single line comment
+
+foo/* this is 
+a multiline 
+comment */bar
 
 42
 4_2
@@ -19,3 +36,22 @@ test; 67;
 0x_67_7a_2f_cc_40_c6
 170141183460469231731687303715884105727
 170_141183_460469_231731_687303_715884_105727
+
+0.
+72.40
+072.40
+2.71828
+.25
+1_5.
+
+0i
+0123i
+0o123i
+0xabci
+0.i
+2.71828i
+.25i
+
+/* 
+test for end of stream 
+*/
