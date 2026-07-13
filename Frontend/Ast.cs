7371c03e5397c21af76c7a0cbff34134dc2886f6
red-ornob <red.ornob.dev@gmail.com>
2026-07-13 14:17:17 +0600

    added basic ast nodes to start working on the parser

--- Frontend/Ast.cs
+++ Frontend/Ast.cs
@@ -21,4 +21,41 @@ internal static class Ast
         List<Node> Children,
         string Variable
     ) : Node(Children);
+    
+    internal sealed record IfNode(
+        List<Node> Children,
+        Node Condition,
+        Node Then,
+        Node Else
+    ) : Node(Children);
+    
+    internal sealed record ForNode(
+        List<Node> Children,
+        Node Declaration,
+        Node Condition,
+        Node Increment,
+        Node Body
+    ) : Node(Children);
+    
+    internal sealed record BreakNode(
+        List<Node> Children,
+        Node Scope
+    ) : Node(Children);
+    
+    internal sealed record ContinueNode(
+        List<Node> Children,
+        Node Scope
+    ) : Node(Children);
+    
+    internal sealed record ReturnNode(
+        List<Node> Children,
+        Node Scope
+    ) : Node(Children);
+    
+    internal sealed record FunctionNode(
+        List<Node> Children,
+        Node Identifier,
+        List<Node> Parameters,
+        Node Body
+    ) : Node(Children);
 }
